2821a2b0b31c359383a1eb7f59ff929d2997be92
Mika <Mika@Wisener.dev>
2023-10-06 16:06:50 -0700

    load project pages without full data

--- src/components/projects/ProjectHeader.jsx
+++ src/components/projects/ProjectHeader.jsx
@@ -4,12 +4,12 @@ import { useSingleProject } from '../../context/SingleProjectContext';
 
 function ProjectSingleHeader({ data }) {
 	const { projectData, error } = useSingleProject();
-	if (!data) {
-		return null; // or return null, or a loader component
-	}
-	if (error) {
-		return <p>Error loading project header</p>;
-	}
+	// if (!data) {
+	// 	return null; // or return null, or a loader component
+	// }
+	// if (error) {
+	// 	return <p>Error loading project header</p>;
+	// }
 
 	// if (!projectData) {
 	// 	return <p>Loading...</p>;
@@ -20,19 +20,19 @@ function ProjectSingleHeader({ data }) {
 	return (
 		<div>
 			<p className="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-14 sm:mt-20 mb-7">
-				{ProjectHeader.ProjectHeader.title}
+				{ProjectHeader.title}
 			</p>
 			<div className="flex">
 				<div className="flex items-center mr-10">
 					<FiClock className="text-lg text-ternary-dark dark:text-ternary-light" />
 					<span className="font-general-regular ml-2 leading-none text-primary-dark dark:text-primary-light">
-						{ProjectHeader.ProjectHeader.publishDate}
+						{ProjectHeader.publishDate}
 					</span>
 				</div>
 				<div className="flex items-center">
 					<FiTag className="text-lg text-ternary-dark dark:text-ternary-light" />
 					<span className="font-general-regular ml-2 leading-none text-primary-dark dark:text-primary-light">
-						{ProjectHeader.ProjectHeader.tags}
+						{ProjectHeader.tags}
 					</span>
 				</div>
 			</div>
