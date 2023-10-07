c754e9e2fe44a1ae0a4d95999a83eb831fc62223
Mika <Mika@Wisener.dev>
2023-10-06 20:23:09 -0700

    fix error when linked to single project first

--- src/pages/ProjectSingle.jsx
+++ src/pages/ProjectSingle.jsx
@@ -6,36 +6,42 @@ import ProjectHeader from '../components/projects/ProjectHeader';
 import ProjectInfo from '../components/projects/ProjectInfo';
 import ProjectRelatedProjects from '../components/projects/ProjectRelatedProjects';
 import { SingleProjectProvider } from '../context/SingleProjectContext';
+
 const ProjectSingle = () => {
-	const { projectData, error } = useSingleProject();
+    const { projectData, error } = useSingleProject();
 
-	if (error || !projectData) {
-		return <div>Project not found</div>;
-	}
+    // Loading state
+    if (!projectData && !error) {
+        return <div>Loading...</div>;
+    }
 
-	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1, delay: 1 }}
-			transition={{
-				ease: 'easeInOut',
-				duration: 0.6,
-				delay: 0.15,
-			}}
-			className="container mx-auto mt-5 sm:mt-10"
-		>
-			<ProjectHeader projectData={projectData} />
-			<ProjectGallery projectData={projectData} />
-			<ProjectInfo projectData={projectData} />
-			<ProjectRelatedProjects projectData={projectData} />
-			{/* <SingleProjectProvider>
-				<ProjectHeader />
-				<ProjectGallery />
-				<ProjectInfo />
-				<ProjectRelatedProjects />
-			</SingleProjectProvider> */}
-		</motion.div>
-	);
+    // Error state
+    if (error) {
+        return <div>Error loading project. Please try again later.</div>;
+    }
+
+    // Ensure data is available
+    if (!projectData) {
+        return <div>Project not found</div>;
+    }
+
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, delay: 1 }}
+            transition={{
+                ease: 'easeInOut',
+                duration: 0.6,
+                delay: 0.15,
+            }}
+            className="container mx-auto mt-5 sm:mt-10"
+        >
+            <ProjectHeader projectData={projectData} />
+            <ProjectGallery projectData={projectData} />
+            <ProjectInfo projectData={projectData} />
+            <ProjectRelatedProjects projectData={projectData} />
+        </motion.div>
+    );
 };
 
 export default ProjectSingle;
