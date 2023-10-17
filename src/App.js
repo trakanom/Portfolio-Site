a8f2edce97cbbb3ef422a09a96f9f90e8def8f71
Mika <Mika@Wisener.dev>
2023-10-17 14:01:58 -0700

    Add basic consent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState} from 'react';
+import React, { Suspense, useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ScrollToTop from './components/ScrollToTop';
@@ -9,6 +9,7 @@ import { SingleProjectProvider } from './context/SingleProjectContext';
 import './css/App.css';
 import UnderConstructionSplash from './components/UnderConstructionSplash';
 import GoogleTagManager from './components/GoogleTagManager';
+import ConsentBanner from './components/ConsentBanner';
 // Lazy-loaded components
 const About = React.lazy(() => import('./pages/AboutMe'));
 const Contact = React.lazy(() => import('./pages/Contact'));
@@ -19,12 +20,35 @@ const ProjectSingle = React.lazy(() => import('./pages/ProjectSingle'));
 
 function App() {
 	const [showSplash, setShowSplash] = useState(true);
+    const [showConsentBanner, setShowConsentBanner] = useState(false);
 
+    useEffect(() => {
+        const consentGiven = localStorage.getItem('ga-consent-given');
+        if (consentGiven === null) {
+            setShowConsentBanner(true);
+        }
+    }, []);
+
+    const handleAccept = () => {
+        localStorage.setItem('ga-consent-given', 'true');
+        setShowConsentBanner(false);
+        window.gtag('consent', 'update', {
+            'ad_storage': 'granted',
+            'analytics_storage': 'granted'
+        });
+    };
+
+    const handleDecline = () => {
+        localStorage.setItem('ga-consent-given', 'false');
+        setShowConsentBanner(false);
+        window.gtag('consent', 'update', {
+            'ad_storage': 'denied',
+            'analytics_storage': 'denied'
+        });
+    };
 	return (
 		<div classname="App">
-			
 		<AnimatePresence>
-		
 			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
 				<Router>
 					<GoogleTagManager />
@@ -47,6 +71,7 @@ function App() {
 				</Router>
 				<UseScrollToTop />
 			</div>
+			{showConsentBanner && <ConsentBanner onAccept={handleAccept} onDecline={handleDecline} />}
 			{showSplash && <UnderConstructionSplash onClose={() => setShowSplash(false)} />}
 		</AnimatePresence>
 		</div>
