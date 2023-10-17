c5f473bce348a8b1ed81c2375cea445acebb4309
Mika <Mika@Wisener.dev>
2023-10-17 15:33:45 -0700

    update styling

--- src/components/ConsentBanner.jsx
+++ src/components/ConsentBanner.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function ConsentBanner({ onAccept, onDecline }) {
   return (
-    <div style={styles.banner}>
-      <p>We use cookies to analyze website traffic. Do you consent to our use of cookies for this purpose?</p>
-      <button onClick={onAccept} style={styles.button}>Accept</button>
-      <button onClick={onDecline} style={styles.button}>Decline</button>
-    </div>
+      <div style={styles.banner} className="flex justify-between items-end p-5 border-t border-ternary-light dark:border-ternary-dark">
+        <p>We use cookies to analyze website traffic. Do you consent to our use of cookies for this purpose?</p>
+        <button onClick={onAccept} style={styles.button} className='px-4 py-2 text-white bg-indigo-500 hover:bg-indigo-600 rounded-md focus:ring-1 focus:ring-indigo-900 duration-500'>Accept</button>
+        <button onClick={onDecline} style={styles.button} className='px-4 py-2 text-primary-light  bg-gray-600 hover:bg-ternary-dark dark:bg-gray-200 dark:text-secondary-dark dark:hover:bg-primary-light rounded-md focus:ring-1 focus:ring-indigo-900 duration-500'>Decline</button>
+      </div>
   );
 }
 //TODO: update styles to match site
@@ -19,6 +19,7 @@ const styles = {
     backgroundColor: '#333',
     color: '#fff',
     padding: '1em',
+    margin: '0 10%',
     display: 'flex',
     justifyContent: 'Center',
     alignItems: 'Center',
