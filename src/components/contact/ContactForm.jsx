a827f060d718fca12a5e4c668d403bb19af0f33a
Mika <Mika@Wisener.dev>
2023-10-19 16:47:10 -0700

    update submission logic

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -17,7 +17,39 @@ const ContactForm = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		// Handle the form submission logic here
+
+		// 1. Gather Form Data
+		const formData = new FormData(e.target);
+
+		// 2. Validation (basic example for email)
+		const email = formData.get("email");
+		const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+		if (!emailRegex.test(email)) {
+			alert("Please enter a valid email address.");
+			return;
+		}
+
+		// 3. Send Data
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+		})
+			.then((response) => {
+				if (response.ok) {
+					// 4. Handle Response
+					setIsModalOpen(true); // Show thank you modal
+					e.target.reset(); // Reset the form
+				} else {
+					throw new Error("Network response was not ok");
+				}
+			})
+			.catch((error) => {
+				alert(
+					"There was a problem with the form submission: " +
+						error.message
+				);
+			});
 	};
 
 	return (
