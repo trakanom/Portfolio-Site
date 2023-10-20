53878909328488b5e21d5fa1e193de8f6f1967fe
Mika <Mika@Wisener.dev>
2023-10-19 20:26:29 -0700

    add captcha

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
-import React, { Suspense, useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import ScrollToTop from './components/ScrollToTop';
-import AppFooter from './components/shared/AppFooter';
-import AppHeader from './components/shared/AppHeader';
-import UseScrollToTop from './hooks/useScrollToTop';
-import { SingleProjectProvider } from './context/SingleProjectContext';
-import './css/App.css';
-import UnderConstructionSplash from './components/UnderConstructionSplash';
-import GoogleTagManager from './components/GoogleTagManager';
-import ConsentBanner from './components/ConsentBanner';
-import useAnalyticsConsent from './hooks/useAnalyticsConsent';
+import React, { Suspense, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import ScrollToTop from "./components/ScrollToTop";
+import AppFooter from "./components/shared/AppFooter";
+import AppHeader from "./components/shared/AppHeader";
+import UseScrollToTop from "./hooks/useScrollToTop";
+import { SingleProjectProvider } from "./context/SingleProjectContext";
+import "./css/App.css";
+import UnderConstructionSplash from "./components/UnderConstructionSplash";
+import GoogleTagManager from "./components/GoogleTagManager";
+import ConsentBanner from "./components/ConsentBanner";
+import useAnalyticsConsent from "./hooks/useAnalyticsConsent";
 
 // Lazy-loaded components
-const About = React.lazy(() => import('./pages/AboutMe'));
-const Contact = React.lazy(() => import('./pages/Contact'));
-const Home = React.lazy(() => import('./pages/Home'));
-const Projects = React.lazy(() => import('./pages/Projects'));
-const ProjectSingle = React.lazy(() => import('./pages/ProjectSingle'));
-
+const About = React.lazy(() => import("./pages/AboutMe"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const Home = React.lazy(() => import("./pages/Home"));
+const Projects = React.lazy(() => import("./pages/Projects"));
+const ProjectSingle = React.lazy(() => import("./pages/ProjectSingle"));
 
 function App() {
 	const [showSplash, setShowSplash] = useState(true);
-	const { showConsentBanner, handleAccept, handleDecline } = useAnalyticsConsent();
+	const { showConsentBanner, handleAccept, handleDecline } =
+		useAnalyticsConsent();
 	return (
-		<div classname="App">
-		<AnimatePresence>
-			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-				<Router>
-					<GoogleTagManager />
-					<ScrollToTop />
-					<AppHeader />
-					<Suspense fallback={""}>
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="projects" element={<Projects />} />
-							<Route path="projects/:projectId" element={
-								<SingleProjectProvider>
-									<ProjectSingle />
-								</SingleProjectProvider>
-							} />
-							<Route path="about" element={<About />} />
-							<Route path="contact" element={<Contact />} />
-						</Routes>
-					</Suspense>
-					<AppFooter />
-				</Router>
-				<UseScrollToTop />
-			</div>
-			{showConsentBanner && <ConsentBanner onAccept={handleAccept} onDecline={handleDecline} />}
-			{showSplash && <UnderConstructionSplash onClose={() => setShowSplash(false)} />}
-		</AnimatePresence>
+		<div ClassName="App">
+			<AnimatePresence>
+				<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+					<Router>
+						<GoogleTagManager />
+						<ScrollToTop />
+						<AppHeader />
+						<Suspense fallback={""}>
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="projects" element={<Projects />} />
+								<Route
+									path="projects/:projectId"
+									element={
+										<SingleProjectProvider>
+											<ProjectSingle />
+										</SingleProjectProvider>
+									}
+								/>
+								<Route path="about" element={<About />} />
+								<Route path="contact" element={<Contact />} />
+							</Routes>
+						</Suspense>
+						<AppFooter />
+					</Router>
+					<UseScrollToTop />
+				</div>
+				{showConsentBanner && (
+					<ConsentBanner
+						onAccept={handleAccept}
+						onDecline={handleDecline}
+					/>
+				)}
+				{showSplash && (
+					<UnderConstructionSplash
+						onClose={() => setShowSplash(false)}
+					/>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 }
--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,13 +1,19 @@
-// ContactForm.jsx
+//src/components/contact/ContactForm.jsx
 import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import ReCAPTCHA from "react-google-recaptcha";
 import ThankYouModal from "../ThankYouModal";
+import contactFormSchema from "../../../src/utils/validationSchemas";
 
 const ContactForm = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [formHtml, setFormHtml] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [errors, setErrors] = useState({});
+	const validationSchema = contactFormSchema;
+	const [captchaValue, setCaptchaValue] = useState(null);
 
 	useEffect(() => {
-		// Fetch the form when the component mounts
 		fetch("/contact_form.html")
 			.then((response) => response.text())
 			.then((data) => {
@@ -15,54 +21,111 @@ const ContactForm = () => {
 			});
 	}, []);
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
+		// Prevent the default form submission behavior
 		e.preventDefault();
 
-		// 1. Gather Form Data
-		const formData = new FormData(e.target);
+		// Set the loading state to true to indicate the form is being processed
+		setLoading(true);
 
-		// 2. Validation (basic example for email)
-		const email = formData.get("email");
-		const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-		if (!emailRegex.test(email)) {
-			alert("Please enter a valid email address.");
+		// Check if the reCAPTCHA has been filled out
+		if (!captchaValue) {
+			// If not, set an error for the captcha and stop the form submission
+			setErrors({ captcha: "Please verify you are not a robot." });
+			setLoading(false);
 			return;
 		}
 
-		// 3. Send Data
-		fetch("/", {
-			method: "POST",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: new URLSearchParams(formData).toString(),
-		})
-			.then((response) => {
-				if (response.ok) {
-					// 4. Handle Response
-					setIsModalOpen(true); // Show thank you modal
-					e.target.reset(); // Reset the form
-				} else {
-					throw new Error("Network response was not ok");
-				}
-			})
-			.catch((error) => {
-				alert(
-					"There was a problem with the form submission: " +
-						error.message
-				);
+		// Create a FormData object from the form element to easily gather the form data
+		const formData = new FormData(e.target);
+
+		// Extract individual form values for validation
+		const formValues = {
+			name: formData.get("name"),
+			email: formData.get("email"),
+			message: formData.get("message"),
+		};
+
+		try {
+			// Validate the form values using the validation schema
+			await validationSchema.validate(formValues, { abortEarly: false });
+
+			// If validation is successful, clear any previous errors
+			setErrors({});
+
+			// Send the form data to the server (in this case, it's being sent to the root path '/')
+			const response = await fetch("/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: new URLSearchParams(formData).toString(),
 			});
+
+			// Check if the server responded with a success status
+			if (!response.ok) {
+				// If not, throw an error to be caught in the catch block
+				throw new Error("Network response was not ok");
+			}
+
+			// If the form was submitted successfully, show the thank you modal and reset the form
+			setIsModalOpen(true);
+			e.target.reset();
+		} catch (error) {
+			// Check if the error is a validation error
+			if (error instanceof yup.ValidationError) {
+				// If it is, transform the validation errors into a more usable format
+				const errorMessages = {};
+				error.inner.forEach((err) => {
+					errorMessages[err.path] = err.message;
+				});
+				// Set the errors to state to be displayed on the form
+				setErrors(errorMessages);
+			} else {
+				// If it's not a validation error, it's a network or other error. Handle it accordingly.
+				setErrors({
+					form:
+						"There was a problem with the form submission: " +
+						error.message,
+				});
+			}
+		}
+
+		// Set the loading state back to false to indicate the form processing is done
+		setLoading(false);
 	};
 
 	return (
 		<div className="w-full lg:w-1/2">
 			<div className="leading-loose">
-				<div
+				<form
 					onSubmit={handleSubmit}
-					dangerouslySetInnerHTML={{ __html: formHtml }}
-				/>
-				<ThankYouModal
-					isOpen={isModalOpen}
-					onClose={() => setIsModalOpen(false)}
-				/>
+					data-netlify="true"
+					name="contact"
+				>
+					<div dangerouslySetInnerHTML={{ __html: formHtml }}></div>
+					{/* ... (rest of the form fields) */}
+					<ReCAPTCHA
+						sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+						onChange={(value) => setCaptchaValue(value)}
+					/>
+					{errors.captcha && (
+						<div className="text-red-500">{errors.captcha}</div>
+					)}
+					<div className="font-general-medium w-40 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
+						<button
+							type="submit"
+							aria-label="Send Message"
+							disabled={loading}
+						>
+							{loading ? "Sending..." : "Send Message"}
+						</button>
+					</div>
+					<ThankYouModal
+						isOpen={isModalOpen}
+						onClose={() => setIsModalOpen(false)}
+					/>
+				</form>
 			</div>
 		</div>
 	);
