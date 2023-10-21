68fb30089e218cff670abbfc43f160f046ee4526
Mika <Mika@Wisener.dev>
2023-10-20 18:41:43 -0700

    change dark-theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
 				"secondary-light": "#FFFFFF",
 				"ternary-light": "#f6f7f8",
 
-				"primary-dark": "#0D2438",
-				"secondary-dark": "#102D44",
-				"ternary-dark": "#1E3851",
+				"primary-dark": "#290025",
+				"secondary-dark": "#3A015C",
+				"ternary-dark": "#4F0147",
 			},
 			container: {
 				padding: {
